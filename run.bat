0cfc81a38a45f727156b5cafb54bd9848bc4e04a
Brian LaFond <52360893+iroxusux@users.noreply.github.com>
2024-06-21 08:33:32 -0400

    update run.bat to auto-update repos

--- run.bat
+++ run.bat
@@ -1,7 +1,9 @@
 @echo on
 
-call .\venv\Scripts\activate.bat
+call .\.venv\Scripts\activate.bat
 
+python -m pip install --upgrade PyDiscoBot
+python -m pip install --upgrade MLEBot
 python myBot.py
 
 PAUSE
